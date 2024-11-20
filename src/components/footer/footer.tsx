4dd15d22e1154eb18deb5deb100710c514ca70af
JessicaMoscato <jessica.moscato@oclock.school>
2024-11-20 11:46:56 +0100

    mise en page couleur et page accueil

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,7 +1,7 @@
-
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons"; 
+import { NavLink } from "react-router-dom"; // Import de NavLink
 import "./footer.scss"; 
 
 const Footer: React.FC = () => {
@@ -25,6 +25,15 @@ const Footer: React.FC = () => {
             <FontAwesomeIcon icon={faInstagram} className="social-icon" />
           </a>
         </div>
+
+        {/* Remplacer <a> par <NavLink> pour la gestion des liens actifs */}
+        <NavLink
+          to="/contact"
+          className={({ isActive }) => (isActive ? "nav-link active-link" : "nav-link")}
+        >
+          Contact
+        </NavLink>
+
         <div className="copyright">
           {/* Texte de copyright */}
           <p>&copy; 2024 Pet Foster Connect. Tous droits réservés.</p>
